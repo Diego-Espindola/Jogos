f1f5aa2738608e4bdf01790a9db26cfbc948ffba
Diego Espindola <diegoespindola9404@gmail.com>
2021-08-31 12:17:37 -0300

    Update teste_conex_com_bd.py
    Funcionoou!

--- Testando/teste_conex_com_bd.py
+++ Testando/teste_conex_com_bd.py
@@ -2,10 +2,9 @@ import mysql.connector
 
 con = mysql.connector.connect(host='localhost',database='Server_Forca',user='root',password='')
 
-while True:
-    if con.is_connected():
-        db_info = con.get_server_info()
-        print(' O banco está conectado, versão',db_info)
+if con.is_connected():
+    db_info = con.get_server_info()
+    print(' O banco está conectado, versão',db_info)
 
 #from mysql.connector import baseforca
 
