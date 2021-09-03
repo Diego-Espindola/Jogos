966bdf69a421b82c2dbbf85ddafeca592817af43
Diego Espindola <diegoespindola9404@gmail.com>
2021-09-03 12:21:15 -0300

    testes

---
+++ nim.py
@@ -0,0 +1,17 @@
+"""
+
+Nim é um jogo matématico criado pelos chineses
+
+Nesse jogo utiliza-se palitos ou qualquer objeto
+
+É necessário 21 peças e 2 jogadores
+
+As peças devem ficar enfileiradas 
+
+O jogador poderá tirar 1, 2 ou 3 peças de qualquer lugar na sua vez de jogar
+
+O jogador que retirar a última peça perde
+
+"""
+
+
