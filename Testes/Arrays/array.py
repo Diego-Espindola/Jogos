30e5830e74d7fda6916a73dc71e987c1288b165c
Diego Espindola <diegoespindola9404@gmail.com>
2021-09-06 07:01:38 -0300

    Update array.py

--- Testes/Arrays/array.py
+++ Testes/Arrays/array.py
@@ -1,38 +1,59 @@
 import numpy as np
 print('Criando manualmente')
 a = np.array([0, 1, 2, 3])
-print('1º array\n', a)
+print('a: array\n', a)
 
 b = np.array([[0, 1, 2], [3, 4, 5], [5, 6, 7], [8,9,10]])
 
 #possui duas linhas, cada lista interna é uma linha, e cada item é uma coluna 4x3
 
-print('array com lista bidimensional\n', b)
+print('b:array com lista bidimensional\n', b)
+print()
 
 
 #como saber a dimensão do array
-print(b.ndim, 'Array bidimensional')
-print(a.ndim, 'Array unidimensional')
-
+print(b.ndim, 'b.ndim: Array bidimensional')
+print()
+print(a.ndim, 'a.ndim: Array unidimensional')
+print()
 #método para visualizar o formato do array
-print('Linhas e colunas', b.shape)
-
+print('b.shape: Linhas e colunas', b.shape)
+print()
 #função para saber o tamanho(primeira dimensão) Mais vale a utilização do .shape
-print('Número de linhas', len(b))
-
+print('len(b): Número de linhas', len(b))
+print()
 
 
 print('\n criar automáticamente\n')
 #Criar um array com um determinado número de elementos
 c = np.arange(0, 11, 1.5)
 #np.arange(início, fim(não inclui o número informado), iteração(pode ser float))
-print('Array automático, range de 0 a 99\n', c)
-
+print('c: Array automático, range de 0 a 11 com um intervalo de 1.5\n', c)
+print()
 #Também permite criar um array uniformemente espaçado, mas diferente do arange vc diz quantos elementos vc quer em um intervalo
 d = np.linspace(1, 10, 13)
 #(início, término(inclui o número informado), número de itens) ..... se quiser que não inclua o número informado no "término" usar(inicio, término, número de itens, endpoint = False)
 
-print('Array automático entre 1 a 10 com 13 elementos entre eles\n', d)
+print('d: Array automático entre 1 a 10 com 13 elementos entre eles\n', d)
+print()
+
+#Criar array de 1s
+
+e = np.ones(10)
+print('e: Array de 10 1s', e)
+print()
+
+#Criar array que seja uma matriz
+f = np.ones((3,3))
+print('f: Array Matriz 3x3\n', f)
+print()
+
+#da mesma forma com zeros, pode trocarnp.ones por np.zeros (Funciona tanto uni qnt bidimensional)
+
+g = np.zeros(4)
+print('g: Array unidimensional de zeros\n', g)
+print()
+
 
 
 
